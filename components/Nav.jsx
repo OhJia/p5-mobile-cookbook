6213580740c1149915fe25b0cd5773937f3f7ac6
OhJia <jiajia0919@gmail.com>
2015-08-19 18:11:33 -0400

    reverse back to Link

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,15 +1,13 @@
 var React = require('react')
-//var Router = require('react-router')
-//var Link = Router.Link
+var Router = require('react-router')
+var Link = Router.Link
 
 var Nav = React.createClass({
   render: function () {
     return (
       <header>
-        // <Link to='/'>Index</Link>
-        // <Link to='/getting-started'>Getting Started</Link>
-        <a href='/'>Index</a>
-        <a href='/getting-started'>Getting Started</a>
+        <Link to='/'>Index</Link>
+        <Link to='/getting-started'>Getting Started</Link>
       </header>
     )
   }
